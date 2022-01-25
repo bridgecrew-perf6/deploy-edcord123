65892df3205c5821ee928d8747a9cb78ae828f61
Edwin Joseph <edwinjoseph241@gmail.com>
2022-01-25 13:41:41 +0530

    settings whitenoise changed

--- studybud/settings.py
+++ studybud/settings.py
@@ -134,7 +134,10 @@ STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
