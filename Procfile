8ad44d5af617aa73d3a65625cb6619c9fbc635c5
Edwin Joseph <edwinjoseph241@gmail.com>
2022-01-25 13:03:15 +0530

    deploy commit2

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn studybud.wsgi --log-file -
+web: gunicorn studybud.wsgi 
+
--- studybud/settings.py
+++ studybud/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+
 import os
+
 from pathlib import Path
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +140,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
